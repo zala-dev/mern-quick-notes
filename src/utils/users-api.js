285b3b3f43ca7c9758d2661b15b794eafb5969a4
Zala <zala.developer@gmail.com>
2024-07-30 18:30:52 +0930

    refactor api calls

--- src/utils/users-api.js
+++ src/utils/users-api.js
@@ -1,29 +1,37 @@
+import sendRequest from "./send-request";
 const BASE_URL = "/api/users";
 
-export async function signUp(userData) {
-  const res = await fetch(BASE_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userData),
-  });
+// export async function signUp(userData) {
+//   const res = await fetch(BASE_URL, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(userData),
+//   });
 
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Invalid Sign Up");
-  }
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Invalid Sign Up");
+//   }
+// }
+
+// export async function login(credentials) {
+//   const res = await fetch(`${BASE_URL}/login`, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(credentials),
+//   });
+
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Invalid Login");
+//   }
+// }
+
+export function signUp(userData) {
+  return sendRequest(BASE_URL, "POST", userData);
 }
-
-export async function login(credentials) {
-  const res = await fetch(`${BASE_URL}/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(credentials),
-  });
-
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Invalid Login");
-  }
+export function login(credentials) {
+  return sendRequest(`${BASE_URL}/login`, "POST", credentials);
 }
